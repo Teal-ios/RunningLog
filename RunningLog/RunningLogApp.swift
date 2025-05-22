550a38e70d3cc13e06b8db641569c0d9190e69c5
Den <qudgus1984@naver.com>
2025-05-22 13:20:33 +0900

    Initial Commit

---
+++ RunningLog/RunningLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  RunningLogApp.swift
+//  RunningLog
+//
+//  Created by Den on 5/22/25.
+//
+
+import SwiftUI
+
+@main
+struct RunningLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RunningLogTests/RunningLogTests.swift
@@ -0,0 +1,17 @@
+//
+//  RunningLogTests.swift
+//  RunningLogTests
+//
+//  Created by Den on 5/22/25.
+//
+
+import Testing
+@testable import RunningLog
+
+struct RunningLogTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
